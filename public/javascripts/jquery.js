609b648ecffb56d57cc44e078a2576a4887bad5a
Faye Burke <fayeelizabethburke@gmail.com>
2019-10-24 20:04:36 +1100

    add iframes

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -4,6 +4,30 @@ $(document).ready(function(){
         $(".containerOne").slideUp(300);
     });
 
+    $('#idToHoverOne').hover(function(){
+        // $('#idModal').css("display", "block");
+        $(this).replaceWith( ('<iframe src="/videos/planB.mp4" width=750px height=380px; allow="autoplay"></iframe>') );
+      })
+
+      $('#idToHoverTwo').hover(function(){
+        // $('#idModal').css("display", "block");
+        $(this).replaceWith( ('<iframe src="/videos/codeSnip.mp4" width=750px height=380px; allow="autoplay"></iframe>') );
+      })
+
+      $('#idToHoverTwo').hover(function(){
+        // $('#idModal').css("display", "block");
+        $(this).replaceWith( ('<iframe src="/videos/codeSnip.mp4" width=750px height=380px; allow="autoplay"></iframe>') );
+      })
+
+      $('#idToHoverTwo').hover(function(){
+        // $('#idModal').css("display", "block");
+        $(this).replaceWith( ('<h2>UNDER CONSTRUCTION</h2>') );
+      })
+      
+      $('#idToHover').mouseout(function(){
+        $('iframe').hide();
+      })
+
     $("#go").click(function(event){
         $(".partial").show(200);
         $(".bio").hide();
